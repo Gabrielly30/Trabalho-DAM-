e3eeab1dcf861f2dd5ad2b1a7b9ac27b5e4f510c
Gabrielly30 <72361638+Gabrielly30@users.noreply.github.com>
2020-10-04 20:04:55 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, View  } from 'react-native';
+import Tarefa from './Tarefa';
 
 export default function App() {
+
+  const [tarefas, SetTarefas]= useState([
+    {id:1, title:'Atualizar seus dados;'},
+    {id:2, title:'Conceder nota para a atividade;'},
+    {id:3, title:'Criar questões para os skills'},
+    {id:4, title:'Visualizar as avaliações recebidas'},
+    {id:5, title:'Relizar pesquisa de feedback 360º',
+    }
+  
+  ]);
+  
+  function handleFinalizado(tarefaId){
+    {alert ('Já foi concluído essa ação')}
+    const tarefas_pendentes= tarefas.filter(({id})=>{ return id !== tarefaId})
+     
+    SetTarefas(tarefas_pendentes)
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+   
+     {tarefas.map((tarefas)=>
+      
+       <Tarefa 
+       key={tarefas.id}
+       tarefas={tarefas}
+       handleFinalizado={handleFinalizado}/>
+     )}
+  
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +41,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#5555',
     alignItems: 'center',
     justifyContent: 'center',
   },
